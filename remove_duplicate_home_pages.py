2a7bb6fab3744e1235e68bbb6d9e100627b9f9ed
coati-00 <coati.00@gmail.com>
2015-10-23 16:34:02 -0400

    find num of lowest value

--- remove_duplicate_home_pages.py
+++ remove_duplicate_home_pages.py
@@ -1,42 +1,68 @@
 '''
-  Go through all elements of the directory tree, if the folder's name is layout,
-  go through all html files in that folder and search for html files with _pid in
-  their name, these files should be deleted. After deleting these files, go through
-  the text of the remaining html files looking for links to files with _pid in the
-  name, update these strings to remove _pid from name
+  Find home page duplicates and delete them, then update links to the duplicate
 '''
 from subprocess import call
 import os
 import sys
 import fileinput
+import re
 
 
 file_to_search = sys.argv[1]
 
 
 def search_folders(file_name):
+    orig_homepage = "case_id_\d+.html"
+    dup_homepage = "case_id_\d+_id_\d+.html"
     for path, subdirs, files in os.walk(file_name):
-        files.sort()
-        for f_name in files:
-            '''Remove duplicate html files ending with _pid_0.html'''
-            if "_pid_0" in f_name:
-                print "Removing file : " + str(f_name)
-                file_path = os.path.join(path, f_name)
-                call(["git", "rm", file_path])
+        if path.endswith("layout"):
+            files.sort()
+            potential_dups = []
+            for f_name in files:
+                homematchObj = re.match(orig_homepage, f_name)
+                dupmatchObj = re.match(dup_homepage, f_name)
+                if homematchObj:
+                    # print "match found for homepage file: "
+                    print f_name
+                else:
+                    pass
+                    # print "Not a homepage file"
+                    # print f_name
+                if dupmatchObj:
+                    # print "match found duplicate homepage file: "
+                    print f_name[:-5].split('_')[4]
+                    potential_dups.append(f_name[:-5].split('_')[4])
+                    temp = potential_dups[0]
+                    for each in potential_dups:
+                        if each < temp:
+                            temp = each
+                    print "lowest value file is"
+                    print temp
+                        
+                else:
+                    pass
+                    # print "Not homepage duplicate"
+                    # print f_name
+                    
+#                 '''Find home page, only has one id_*num*'''
+#                 if "_pid_0" in f_name:
+#                     print "Removing file : " + str(f_name)
+#                     file_path = os.path.join(path, f_name)
+#                     call(["git", "rm", file_path])
 
-    # go over directory second time - # of files
-    # to update should be smaller
-    for path, subdirs, files in os.walk(file_name):
-        files.sort()
-        for f_name in files:
-            if "case_id_" in f_name:
-                print "Updating file " + str(f_name)
-                file_path = os.path.join(path, f_name)
-                old_string = "_pid_0.html"
-                new_string = ".html"
-                newfile = open(file_path, 'r+')
-                for line in fileinput.input(file_path):
-                    newfile.write(line.replace(old_string, new_string))
-                newfile.close()
+#     # go over directory second time - # of files
+#     # to update should be smaller
+#     for path, subdirs, files in os.walk(file_name):
+#         files.sort()
+#         for f_name in files:
+#             if "case_id_" in f_name:
+#                 print "Updating file " + str(f_name)
+#                 file_path = os.path.join(path, f_name)
+#                 old_string = "_pid_0.html"
+#                 new_string = ".html"
+#                 newfile = open(file_path, 'r+')
+#                 for line in fileinput.input(file_path):
+#                     newfile.write(line.replace(old_string, new_string))
+#                 newfile.close()
 
 search_folders(file_to_search)
